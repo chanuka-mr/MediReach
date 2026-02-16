4e73c6caabc3447a9904c83fd4a84e5abb808bee
Chanuka Madhushan <jchanukamr@gmail.com>
2026-02-16 10:51:34 +0530

    feat: Initialize backend with Express, MongoDB connection, and .env configuration.

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
-console.log("Testing");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use("/", (req, res) => {
+    res.send("Success");
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Server is running on port", process.env.PORT);
+    })
+})
+.catch((err) => {
+    console.log(err);
+})
