36b785ad13461e5cf367b973622b16311443aaed
Thisara Kanishka <thisarailesinghe@gmail.com>
2026-03-27 11:20:32 +0530

    my part done

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const dns = require('dns');
 
 dotenv.config();
+
+// Fix for ECONNREFUSED when resolving SRV records in some networks
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDB = async () => {
     try {
